095a5fc0fcf6bb61ca8d1709bf73d36333665dd9
ohkandil <omarhkandil@gmail.com>
2023-07-18 09:16:43 -0700

    Final final try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
-/**
- * main.c
- *
- * This program prints the characters "_putchar" to the console.
- *
- * @author okandil
- * @return 0 on success
- */
-
 #include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by ohkandil
+	 * Return: 0
+	 */
 
-int main(void)
-{
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
 
-  return 0;
-}
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
